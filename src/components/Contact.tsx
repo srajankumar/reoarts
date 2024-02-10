a51a569f3c3aacde57e0e619847fc2e8489e7d81
srajan <kumarsrajan02@gmail.com>
2024-02-10 19:24:55 +0530

    add: framer motion

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -2,6 +2,7 @@
 
 import React, { useState } from "react";
 import emailjs from "emailjs-com";
+import { motion, useInView } from "framer-motion";
 
 import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
@@ -16,6 +17,19 @@ const Contact = () => {
   const [email, setEmail] = useState("");
   const [subject, setSubject] = useState("");
   const [message, setMessage] = useState("");
+
+  const ref = React.useRef(null);
+  const isInView = useInView(ref) as boolean;
+
+  const FADE_DOWN_ANIMATION_VARIANTS = {
+    hidden: { opacity: 0, y: -10 },
+    show: { opacity: 1, y: 0, transition: { type: "spring" } },
+  };
+
+  const FADE_UP_ANIMATION_VARIANTS = {
+    hidden: { opacity: 0, y: +10 },
+    show: { opacity: 1, y: 0, transition: { type: "spring" } },
+  };
 
   const handleSubmit = async (e: { preventDefault: () => void }) => {
     e.preventDefault();
@@ -48,20 +62,53 @@ const Contact = () => {
   };
 
   return (
-    <div className="relative justify-center xl:rounded-t-[20rem] lg:rounded-t-[15rem] md:rounded-t-[12rem] sm:rounded-t-[10rem] rounded-t-[7rem] flex bg-[#0d0d0d] min-h-screen ">
+    <motion.div
+      initial="hidden"
+      className="relative justify-center xl:rounded-t-[20rem] lg:rounded-t-[15rem] md:rounded-t-[12rem] sm:rounded-t-[10rem] rounded-t-[7rem] flex bg-[#0d0d0d] min-h-screen "
+      ref={ref}
+      animate={isInView ? "show" : "hidden"}
+      viewport={{ once: true }}
+      variants={{
+        hidden: {},
+        show: {
+          transition: {
+            staggerChildren: 0.15,
+          },
+        },
+      }}
+    >
       <div className="absolute bottom-0 right-0 w-[24.9%] h-[34.9%] bg-white" />
       <div className="absolute bottom-0 right-0 w-[25%] h-[35%] bg-[#0d0d0d] rounded-br-full z-10" />
       <div className="flex tracking-wide px-8 md:px-10">
         <main className="max-w-6xl py-10">
           <div className="min-h-screen flex justify-center flex-col">
-            <h1 className="text-secondary text-4xl font-extrabold">CONTACT</h1>
-            <div className="py-3 md:w-3/4 text-white">
+            <motion.h1
+              variants={FADE_DOWN_ANIMATION_VARIANTS}
+              className="text-secondary text-4xl font-extrabold"
+            >
+              CONTACT
+            </motion.h1>
+
+            <motion.h1
+              variants={FADE_DOWN_ANIMATION_VARIANTS}
+              className="py-3 md:w-3/4 text-white"
+            >
               Gift a touch of art! For personalized portraits or inquiries about
-              my work, reach out to me. Let&apos;s chat about bringing beauty into
-              your space!
-            </div>
+              my work, reach out to me. Let&apos;s chat about bringing beauty
+              into your space!
+            </motion.h1>
+
+            <motion.h1 variants={FADE_DOWN_ANIMATION_VARIANTS}>
+              Gift a touch of art! For personalized portraits or inquiries about
+              my work, reach out to me. Let&apos;s chat about bringing beauty
+              into your space!
+            </motion.h1>
+
             <form onSubmit={handleSubmit} className="z-50">
-              <div className="flex pb-3 md:flex-row flex-col md:space-x-3 md:space-y-0 space-y-3">
+              <motion.h1
+                variants={FADE_UP_ANIMATION_VARIANTS}
+                className="flex pb-3 md:flex-row flex-col md:space-x-3 md:space-y-0 space-y-3"
+              >
                 <Input
                   type="text"
                   value={name}
@@ -78,27 +125,31 @@ const Contact = () => {
                   id="email"
                   placeholder="Email"
                 />
-              </div>
-              <Textarea
-                required
-                value={message}
-                onChange={(e) => setMessage(e.target.value)}
-                id="message"
-                className="z-10"
-                placeholder="Type your message here"
-              />
-              <Button
-                className="mt-3 px-10 rounded-full"
-                variant="secondary"
-                type="submit"
-              >
-                Send
-              </Button>
+              </motion.h1>
+              <motion.h1 variants={FADE_UP_ANIMATION_VARIANTS}>
+                <Textarea
+                  required
+                  value={message}
+                  onChange={(e) => setMessage(e.target.value)}
+                  id="message"
+                  className="z-10"
+                  placeholder="Type your message here"
+                />
+              </motion.h1>
+              <motion.h1 variants={FADE_UP_ANIMATION_VARIANTS}>
+                <Button
+                  className="mt-3 px-10 rounded-full"
+                  variant="secondary"
+                  type="submit"
+                >
+                  Send
+                </Button>
+              </motion.h1>
             </form>
           </div>
         </main>
       </div>
-    </div>
+    </motion.div>
   );
 };
 
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,24 +1,59 @@
+"use client";
+import React from "react";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
+import { motion, useInView } from "framer-motion";
+
 const Hero = () => {
+  const ref = React.useRef(null);
+  const isInView = useInView(ref) as boolean;
+
+  const FADE_DOWN_ANIMATION_VARIANTS = {
+    hidden: { opacity: 0, y: -10 },
+    show: { opacity: 1, y: 0, transition: { type: "spring" } },
+  };
   return (
-    <div className="min-h-screen md:p-5 p-3 flex justify-center flex-col">
-      <h1 className="text-secondary text-6xl md:text-8xl font-extrabold">
+    <motion.div
+      initial="hidden"
+      className="min-h-screen md:p-5 p-3 flex justify-center flex-col"
+      ref={ref}
+      animate={isInView ? "show" : "hidden"}
+      viewport={{ once: true }}
+      variants={{
+        hidden: {},
+        show: {
+          transition: {
+            staggerChildren: 0.15,
+          },
+        },
+      }}
+    >
+      <motion.h1
+        className="text-secondary text-6xl md:text-8xl font-extrabold"
+        variants={FADE_DOWN_ANIMATION_VARIANTS}
+      >
         REO ARTS
-      </h1>
-      <div className="pt-3 md:w-3/4 text-white">
+      </motion.h1>
+
+      <motion.h1
+        className="pt-3 md:w-3/4 text-white"
+        variants={FADE_DOWN_ANIMATION_VARIANTS}
+      >
         Explore an exquisite collection featuring stunning artistic creations,
         capturing the essence of creativity and passion in every piece.
-      </div>
-      <Link href="#contact">
-        <Button
-          variant="secondary"
-          className="rounded-full w-fit px-10 font-bold mt-5"
-        >
-          Contact
-        </Button>
-      </Link>
-    </div>
+      </motion.h1>
+
+      <motion.h1 variants={FADE_DOWN_ANIMATION_VARIANTS}>
+        <Link href="#contact">
+          <Button
+            variant="secondary"
+            className="rounded-full w-fit px-10 font-bold mt-5"
+          >
+            Contact
+          </Button>
+        </Link>
+      </motion.h1>
+    </motion.div>
   );
 };
 
